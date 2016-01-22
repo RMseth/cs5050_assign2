6ec1d1eb01ccc559263b872bd4d5581fdd92d136
Seth Bertlshofer <seth.b@aggiemail.usu.edu>
2016-01-21 18:07:24 -0700

    fixed part1, working on part2 with the cache setting

--- main.cpp
+++ main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-// pre-initialize vector
-void setup( std::vector<std::vector<std::vector<bool>>>& cache ){
-    cache.resize(3); // resize the top level vector to 3 tall (we only have 3 piles)
-
-    for(int i = 0; i < 3; ++i){
-        for(int j = 0; j < 3; ++j){
-            for(int k=0; k < 3; ++k){
-                cache[i][j][k] = ((i==0 && j==0) || (i==0 && k==0) || (k == 0 && j==0)) ? true : false;
-            }
-        }
-    }
-}
+//// pre-initialize vector
+//void setup( std::vector<std::vector<std::vector<bool>>>& cache ){
+//    cache.resize(3); // resize the top level vector to 3 tall (we only have 3 piles)
+//
+//    for(int i = 0; i < 3; ++i){
+//        for(int j = 0; j < 3; ++j){
+//            for(int k=0; k < 3; ++k){
+//                cache[i][j][k] = ((i==0 && j==0) || (i==0 && k==0) || (k == 0 && j==0)) ? true : false;
+//            }
+//        }
+//    }
+//}
 
 // simple recursive function
 bool winRecursive(int a, int b, int c){
-  if((a==0 && b==0) || (a==0 && c==0) || (b==0 && c==0)){
-    return true;
-  }
-    for(int i = 1; i < a+1; ++i ){
-        return (!winRecursive(i-1, b, c));
+    if(a==0 && b==0 &&  c==0) return false;
+
+    for(int i = 0; i < a; ++i ){
+        return (!winRecursive(a - i, b, c));
     }
 
-    for(int j = 1; j < b+1; ++j ){
-        return (!winRecursive(a, j-1, c));
+    for(int j = 0; j < b; ++j ){
+        return (!winRecursive(a, b - j, c));
     }
 
-    for(int k = 1; k < c+1; ++k ){
-        return (!winRecursive(a, b, k-1));
+    for(int k = 0; k < c; ++k ){
+        return (!winRecursive(a, b, c - k));
     }
 }
 
@@ -36,17 +36,27 @@ bool winRecursive(int a, int b, int c){
 bool winRecursiveWithMemoizing(int a, int b, int c, std::vector<std::vector<std::vector<bool>>>& cache){
 
     // need to figure out my base cases here
+    for(int i = 0; i < a; ++i){
+        for(int j = 0; j < b; ++j){
+            for(int k = 0; k < c; ++k){
+                if(a == 0 && b == 0 && c == 0) cache[a][b][c] = false;
+//                if(winRecursiveWithMemoizing( a - i, b, c, cache ) == cache[i][j][k]) return cache[i][j][k];
+//                if(winRecursiveWithMemoizing( a, b - j, c, cache ) == cache[i][j][k]) return cache[i][j][k];
+//                if(winRecursiveWithMemoizing( a, b, c - k, cache ) == cache[i][j][k]) return cache[i][j][k];
 
-    for(int i = 0; i < a; ++i ){
-        return (!winRecursiveWithMemoizing(i-1, b, c, cache));
-    }
+                for(int i = 0; i < a; ++i ){
+                    return (!winRecursiveWithMemoizing(a - i, b, c, cache));
+                }
 
-    for(int j = 0; j < b; ++j ){
-        return (!winRecursiveWithMemoizing(a, j-1, c, cache));
-    }
+                for(int j = 0; j < b; ++j ){
+                    return (!winRecursiveWithMemoizing(a, b - j, c, cache));
+                }
 
-    for(int k = 0; k < c; ++k ){
-        return (!winRecursiveWithMemoizing(a, b, k-1, cache));
+                for(int k = 0; k < c; ++k ){
+                    return (!winRecursiveWithMemoizing(a, b, c - k, cache));
+                }
+            }
+        }
     }
 }
 
@@ -63,17 +73,37 @@ int main() {
 
     srand(time(NULL));
     // setup 3 random numbers between 1 and 1000
+    num1 = rand() % 10 + 1;
+    num2 = rand() % 10 + 1;
+    num3 = rand() % 10 + 1;
+    std::cout << "num1: " << num1 << " num2: " << num2 << " num3: " << num3 << std::endl;
+
+    // call recursive function
+//    winRecursive(num1, num2, num3);
+
+    // reset the random numbers
     num1 = rand() % 100 + 1;
     num2 = rand() % 100 + 1;
     num3 = rand() % 100 + 1;
 
-    setup(cache);
+    std::cout << "num1: " << num1 << " num2: " << num2 << " num3: " << num3 << std::endl;
+//    setup(cache);
 
-    std::cout << "num1:" << num1 << " num2:" << num2 << " num3:" << num3 << std::endl;
-    // call recursive function
-    winRecursive(num1, num2, num3);
+    // call memoizing
     winRecursiveWithMemoizing(num1, num2, num3, cache);
 
+    std::cout << "cache contents " << std::endl;
+    for(int i = 0; i < 3; ++i){
+        for(int j = 0; j < 3; ++j){
+            for(int k=0; k < 3; ++k){
+                std::string value = cache[i][j][k]? "true" : " false";
+                std::cout << value << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "\nend of cache contents " << std::endl;
 
     return 0;
 }
