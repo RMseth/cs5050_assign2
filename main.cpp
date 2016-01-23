e4ccb1bb4e409117cf6e82cb960fd57e4ddfb091
Seth Bertlshofer <seth.b@aggiemail.usu.edu>
2016-01-22 19:42:39 -0700

    recursion is now working!

--- main.cpp
+++ main.cpp
@@ -2,74 +2,93 @@
 #include <vector>
 #include <string>
 
-//// pre-initialize vector
-//void setup( std::vector<std::vector<std::vector<bool>>>& cache ){
-//    cache.resize(3); // resize the top level vector to 3 tall (we only have 3 piles)
-//
-//    for(int i = 0; i < 3; ++i){
-//        for(int j = 0; j < 3; ++j){
-//            for(int k=0; k < 3; ++k){
-//                cache[i][j][k] = ((i==0 && j==0) || (i==0 && k==0) || (k == 0 && j==0)) ? true : false;
-//            }
-//        }
-//    }
-//}
-
 // simple recursive function
 bool winRecursive(int a, int b, int c){
-    if(a==0 && b==0 &&  c==0) return false;
+    bool val1 = false, val2 = false, val3 = false;
 
-    for(int i = 0; i < a; ++i ){
-        return (!winRecursive(a - i, b, c));
+    std::cout << "num1: " << a << " num2: " << b << " num3: " << c << std::endl;
+    if( a==0 && b==0 && c==0 ) return false; // if all piles are 0 then we have lost
+    if( a==0 && b==0 || a==0 && c==0 || b==0 && c==0) return true; // if any of the other 2 piles are zero then we have won
+
+    for(int i = 1; i < a+1; ++i ){
+        val1 = (!winRecursive(a - i, b, c));
     }
 
-    for(int j = 0; j < b; ++j ){
-        return (!winRecursive(a, b - j, c));
+    for(int j = 1; j < b+1; ++j ){
+        val2 = (!winRecursive(a, b - j, c));
     }
 
-    for(int k = 0; k < c; ++k ){
-        return (!winRecursive(a, b, c - k));
+    for(int k = 1; k < c+1; ++k ){
+        val3 = (!winRecursive(a, b, c - k));
     }
+
+    return (val1 && val2 && val3);
 }
 
 // recursion with a memoizing algo.
-bool winRecursiveWithMemoizing(int a, int b, int c, std::vector<std::vector<std::vector<bool>>>& cache){
+bool winRecursiveWithMemoizing(int a, int b, int c){
+    std::vector<std::vector<std::vector<bool>>> cache;
+    std::cout << "num1: " << a << " num2: " << b << " num3: " << c << std::endl;
 
-    // need to figure out my base cases here
-    for(int i = 0; i < a; ++i){
-        for(int j = 0; j < b; ++j){
-            for(int k = 0; k < c; ++k){
-                if(a == 0 && b == 0 && c == 0) cache[a][b][c] = false;
-//                if(winRecursiveWithMemoizing( a - i, b, c, cache ) == cache[i][j][k]) return cache[i][j][k];
-//                if(winRecursiveWithMemoizing( a, b - j, c, cache ) == cache[i][j][k]) return cache[i][j][k];
-//                if(winRecursiveWithMemoizing( a, b, c - k, cache ) == cache[i][j][k]) return cache[i][j][k];
+    if( a==0 && b==0 && c==0 ) return false;  // if all piles are 0 then we have lost
+    if( a==0 && b==0) return true; // if any of the other 2 piles are zero then we have won
+    if( a==0 && c==0) return true;
+    if( b==0 && c==0) return true;
 
-                for(int i = 0; i < a; ++i ){
-                    return (!winRecursiveWithMemoizing(a - i, b, c, cache));
-                }
-
-                for(int j = 0; j < b; ++j ){
-                    return (!winRecursiveWithMemoizing(a, b - j, c, cache));
-                }
-
-                for(int k = 0; k < c; ++k ){
-                    return (!winRecursiveWithMemoizing(a, b, c - k, cache));
-                }
-            }
+    for (int i = 1; i < a+1; ++i) {
+        if(!winRecursiveWithMemoizing(a - i, b, c)){
+            cache[a-i][b][c] = winRecursiveWithMemoizing(a - i, b, c);
         }
     }
+
+    for (int j = 1; j < b+1; ++j) {
+        if(!winRecursiveWithMemoizing(a, b - j, c)){
+            cache[a][b - j][c] = winRecursiveWithMemoizing(a, b - j, c);
+        }
+    }
+
+    for (int k = 1; k < c+1; ++k) {
+        if(!winRecursiveWithMemoizing(a, b, c - k)){
+            cache[a][b][c - k] = winRecursiveWithMemoizing(a, b, c - k);
+        }
+    }
+
+    return cache[a][b][c];
 }
 
 // Dynamic function
 bool winDynamic(int a, int b, int c){
+    std::vector<std::vector<std::vector<bool>>> cache;
 
+    std::cout << "num1: " << a << " num2: " << b << " num3: " << c << std::endl;
+
+    for(int i = 0; i < a; ++i){
+        for(int j = 0; j < b; ++j){
+            for(int k = 0; k < c; ++k){
+                if(i == 0 && j == 0 && k == 0) break;
+
+                for(int i = 0; i < a+1; ++i ){
+                    cache[i][j][k] = cache[a - i][b][c];
+                }
+
+                for(int j = 0; j < b+1; ++j ){
+                    cache[i][j][k] = cache[a][b - j][c];
+                }
+
+                for(int k = 0; k < c+1; ++k ){
+                    cache[i][j][k] = cache[a][b][c - k];
+                }
+            }
+        }
+    }
+
+    return cache[a][b][c];
 }
 
 int main() {
 
     // declarations
     int num1, num2, num3;
-    std::vector<std::vector<std::vector<bool>>> cache;
 
     srand(time(NULL));
     // setup 3 random numbers between 1 and 1000
@@ -79,7 +98,9 @@ int main() {
     std::cout << "num1: " << num1 << " num2: " << num2 << " num3: " << num3 << std::endl;
 
     // call recursive function
-//    winRecursive(num1, num2, num3);
+    if(winRecursive(num1, num2, num3)){
+        std::cout << "You win" << std::endl;
+    }
 
     // reset the random numbers
     num1 = rand() % 100 + 1;
@@ -87,23 +108,15 @@ int main() {
     num3 = rand() % 100 + 1;
 
     std::cout << "num1: " << num1 << " num2: " << num2 << " num3: " << num3 << std::endl;
-//    setup(cache);
 
     // call memoizing
-    winRecursiveWithMemoizing(num1, num2, num3, cache);
+//    if(winRecursiveWithMemoizing(num1, num2, num3)){
+//        std::cout << "You win" << std::endl;
+//    }
 
-    std::cout << "cache contents " << std::endl;
-    for(int i = 0; i < 3; ++i){
-        for(int j = 0; j < 3; ++j){
-            for(int k=0; k < 3; ++k){
-                std::string value = cache[i][j][k]? "true" : " false";
-                std::cout << value << " ";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-    }
-    std::cout << "\nend of cache contents " << std::endl;
-
+    // call DP
+//    if(winDynamic(num1, num2, num3)){
+//        std::cout << "You win" << std::endl;
+//    }
     return 0;
 }
